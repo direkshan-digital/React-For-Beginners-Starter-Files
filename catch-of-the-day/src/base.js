862482edb6c65d439b1fc7cfbe76123d4d03f52e
Prafull Kotecha <Prafull.Kotecha@gmail.com>
2018-08-05 10:58:12 -0400

    security changes

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import Firebase from "firebase";
 
 const firebaseApp = Firebase.initializeApp({
-  apiKey: "AIzaSyBTIR8Jo_G5Hmq7TlA6o0EUYuSVqLCWCOo",
-  authDomain: "catch-of-the-day-23516.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-23516.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
